f86cc9d327fde2e055d6892e25f036fed847cb89
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-13 17:20:48 +0330

    feat: add buttons to order modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -64,6 +64,10 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: false });
     }
 
+    purchaseContinueHandler = () => {
+        alert('Continue'); //for now
+    }
+
     render() {
         const disabledInfo = { ...this.state.ingredients };
         for (let key in disabledInfo) {
@@ -72,7 +76,12 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary
+                        ingredients={this.state.ingredients}
+                        price={this.state.totalPrice}
+                        purchaseCanceled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
