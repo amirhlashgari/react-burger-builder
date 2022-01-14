dbdc5267a918aecf6e97c05619f022dc970984a9
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-15 01:41:13 +0330

    feat: create axios instance to send http request to backend add create order request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = { salad: 0.5, bacon: 0.7, meat: 1.3, cheese: 0.4 };
 
@@ -65,7 +66,23 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('Continue'); //for now
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Amirhosein Lashkari',
+                address: {
+                    street: 'RedLight St.',
+                    zipCode: '75867',
+                    country: 'Netherlands'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        };
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(err => console.log(err));
     }
 
     render() {
