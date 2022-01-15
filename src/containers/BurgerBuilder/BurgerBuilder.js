76724e1604fcea9619d552a1d2f61bc0f226b95f
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-15 11:41:06 +0330

    feat: add checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -73,28 +73,15 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Amirhosein Lashkari',
-                address: {
-                    street: 'RedLight St.',
-                    zipCode: '75867',
-                    country: 'Netherlands'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        };
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(err => {
-                this.setState({ loading: false, purchasing: false });
-            });
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        queryParams.push('price=' + this.state.totalPrice);
+        this.props.history.push({
+            pathname: 'checkout',
+            search: '?' + queryParams.join('&')
+        });
     }
 
     render() {
