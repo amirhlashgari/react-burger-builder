c44ca36b3531b9654ca6a623e6ba8a37a0bd062d
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-16 14:15:09 +0330

    refactor: fix two bugs in app

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { navLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import classes from './NavigationItem.module.css';
 
 const navigationItem = (props) => (
     <li className={classes.NavigationItem}>
-        <navLink to={props.link} exact={props.exact} activeClassName={classes.active}>
+        <NavLink to={props.link} exact={props.exact} activeClassName={classes.active}>
             {props.children}
-        </navLink>
+        </NavLink>
     </li>
 );
 
