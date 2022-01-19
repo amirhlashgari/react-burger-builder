ad0ca5a35a6675038367373fa217bca505e4c96b
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-19 17:29:17 +0330

    fix: fix a bug when refreshing checkout page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -16,19 +16,23 @@ class Checkout extends Component {
     }
 
     render() {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ings}
-                    checkoutCanceled={this.checkoutCanceledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}
-                />
-                <Route
-                    path={this.props.match.path + '/contact-data'}
-                    component={ContactData} //because we have price as global state we no longer need to pass it by render method to component 
-                />
-            </div>
-        );
+        let summary = <Redirect to='/' />;
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        checkoutCanceled={this.checkoutCanceledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler}
+                    />
+                    <Route
+                        path={this.props.match.path + '/contact-data'}
+                        component={ContactData} //because we have price as global state we no longer need to pass it by render method to component 
+                    />
+                </div>
+            );
+        }
+        return summary;
     }
 }
 
