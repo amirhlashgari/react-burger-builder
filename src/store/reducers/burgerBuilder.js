87ae8cc1d9cc9a53ca7c1576145eaca2499c9e5a
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-19 11:13:14 +0330

    refactor: change actions to action creator method

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,4 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions/actionTypes';
 
 const initialState = { ingredients: { salad: 0, bacon: 0, cheese: 0, meat: 0 }, totalPrice: 4 };
 const INGREDIENT_PRICES = { salad: 0.5, bacon: 0.7, meat: 1.3, cheese: 0.4 };
