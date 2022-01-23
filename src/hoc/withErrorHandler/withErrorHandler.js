65afca20e7d34a6bcc43743da0f1183c64923d7b
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-01-24 02:41:08 +0330

    refactor: convert withErrorHandler component to functional component

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,46 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Aux from '../Aux/Aux';
 import Modal from '../../components/UI/Modal/Modal';
 
 const withErrorHandler = (WrappedComponent, axios) => {
-    return class extends Component {
-        state = {
-            error: null
-        }
+    return props => {
+        const [error, setError] = useState(null);
 
-        componentWillMount() {
-            this.reqInterceptor = axios.interceptors.request.use(req => {
-                this.setState({ error: null });
-                return req;
-            });
-            this.resInterceptor = axios.interceptors.response.use(res => res, err => {
-                this.setState({ error: err });
-            });
-        }
+        const errorConfirmedHandler = () => {
+            setError(null);
+        };
 
-        componentWillUnmount() {
-            axios.interceptors.request.eject(this.reqInterceptor);
-            axios.interceptors.response.eject(this.resInterceptor);
-        }
+        /* we use "componentWillMount" in class based components to run this methods before rendering components,
+           but in functional components we can't turn that to react hooks by "useEffect".
+           we have another way to perform that, as every code before "return" in script,
+           will execute before that, we just use them before returning jsx code */
 
-        errorConfirmedHandler = () => {
-            this.setState({ error: null });
-        }
+        const reqInterceptor = axios.interceptors.request.use(req => {
+            setError(null);
+            return req;
+        });
+        const resInterceptor = axios.interceptors.response.use(res => res, err => {
+            setError(err);
+        });
 
-        render() {
-            return (
-                <Aux>
-                    <Modal
-                        show={this.state.error}
-                        modalClosed={this.errorConfirmedHandler}
-                    >
-                        {this.state.error ? this.state.error.message : null}
-                    </Modal>
-                    <WrappedComponent {...this.props} />
-                </Aux>
-            );
-        }
+        /* using a return function inside "useEffect" will do the cleanup for us,
+           and as we have "componentWillUnmount" in class based we could simply convert that
+           by returning anonymous function inside "useEffect" */
+        useEffect(() => {
+            return () => {
+                axios.interceptors.request.eject(reqInterceptor);
+                axios.interceptors.response.eject(resInterceptor);
+            }
+        }, [reqInterceptor, resInterceptor]);
+
+        return (
+            <Aux>
+                <Modal show={error} modalClosed={errorConfirmedHandler}>
+                    {error ? error.message : null}
+                </Modal>
+                <WrappedComponent {...props} />
+            </Aux>
+        );
     }
 };
 
